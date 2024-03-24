d602628c87dba89a506f317b67e699643a74e16c
Leszek Raczka <leszek.raczka@ponadnorme.pl>
2024-03-24 05:50:34 +0100

    Fix build

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,8 +1,10 @@
 import {ButtonElement} from "@/app/components/Button/styles";
 import Link from "next/link";
+import React from "react";
 
 export const Button = ({linkTo, children}: {
   linkTo?: string,
+  children: React.ReactNode,
 }) => {
   const linkProps = linkTo ? {
     as: Link,
