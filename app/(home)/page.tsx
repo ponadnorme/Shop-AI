3f131a47a4a72e59e3edb550b3229e4b9023baba
Leszek Raczka <leszek.raczka@ponadnorme.pl>
2024-04-08 05:22:18 +0200

    Products store

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,17 +3,7 @@ import {
   ProductSliderAi
 } from '@/app/components/ProductSliderAi/ProductSliderAi';
 import {CenteredContentContainerElement} from '@/app/styles/common';
-
-async function getProducts() {
-  const response = await fetch(`${process.env.NEXT_APP_BASE_URL}/api/list/products`, {
-    cache: 'no-store',
-  });
-  if (!response.ok) {
-    throw new Error('Failed to fetch');
-  }
-
-  return response.json();
-}
+import {getProducts} from '@/app/store/api/products';
 
 export default async function HomePage() {
   const products = await getProducts();
---
+++ app/store/api/products.ts
@@ -0,0 +1,10 @@
+export const getProducts = async () => {
+  const response = await fetch(`${process.env.NEXT_APP_BASE_URL}/api/list/products`, {
+    cache: 'no-store',
+  });
+  if (!response.ok) {
+    throw new Error('Failed to fetch');
+  }
+
+  return response.json();
+}
